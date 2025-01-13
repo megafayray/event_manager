04fb117829032a5c011410ced6ab03122b16e858
megafayray <fay.brotherston@gmail.com>
2025-01-12 22:09:11 -0500

    Create clean phone numbers method

---
+++ lib/event_manager.rb
@@ -0,0 +1,75 @@
+require 'csv'
+require 'google/apis/civicinfo_v2'
+require 'erb'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5,"0")[0..4]
+end
+
+def clean_phone_number(homephone)
+  homephone.gsub!(/\D/, '') #removes any non-digit characters and replaces them with nothing
+
+  if homephone.length < 10
+    homephone = 'bad number- too short'
+  elsif homephone.length > 11
+    homephone = 'bad number - too long'
+  elsif homephone.length == 11
+    if homephone[0]!= 1
+     'bad number'
+    else
+      homephone[0..9]
+    end
+  else 
+    homephone
+  end
+end
+
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    ).officials
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+def save_thank_you_letter(id,form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
+puts 'EventManager initialized.'
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
+contents.each do |row|
+  id = row[0]
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+  legislators = legislators_by_zipcode(zipcode)
+  phone = clean_phone_number(row[:homephone])
+
+  puts "#{id}, #{name}, #{zipcode}, #{phone}"
+
+  # form_letter = erb_template.result(binding)
+
+  # save_thank_you_letter(id,form_letter)
+end
